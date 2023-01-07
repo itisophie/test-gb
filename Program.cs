9dd0770ce3e993d0846c598d6922d6b7851028ff
itisophie <grigorenko.toni@mail.ru>
2023-01-08 00:13:57 +0300

    AddedProblemSolution

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int [] array = new int [6];
-Console.Write($"Задайте массив: ");
-Int.TryParse(Console.ReadLine()!, out new int[]);
+Console.Write($"Введите количество элементов массива: ");
+int elementsCount = int.Parse(Console.ReadLine());
+string[] arrString = new string[elementsCount];
+
+for (int i = 0; i < arrString.Length; i++)
+{
+   Console.Write($"\nВведите элемент массива под индексом:{i}:\t");
+   arrString[i] =  string.Concat(Console.ReadLine());
+}
+Console.WriteLine($"\nВаш массив: ");
+for (int i = 0; i < arrString.Length; i++)
+{
+    Console.WriteLine(arrString[i]);
+}
+Console.ReadLine();
